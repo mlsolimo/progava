be93e05b55fa7ba0e228b44995c4e84bfb9c71c4
LucasSuriano <70494844+LucasSuriano@users.noreply.github.com>
2020-10-20 19:08:35 -0300

    Clase Lanzamiento
    Se implemento la clase Lanzamiento

--- Caber/src/caber/Lanzamiento.java
+++ Caber/src/caber/Lanzamiento.java
@@ -2,4 +2,40 @@ package caber;
 
 public class Lanzamiento {
 
+	private double distancia;
+	private double angulo;
+	private final double anguloMinimoValido = -90;
+	private final double anguloMaximoValido = 90;
+	private final double anguloMinimo100 = -30;
+	private final double anguloMaximo100 = 30;
+	private final double porcentajeDistancia = 0.8;
+	
+	public Lanzamiento(double distancia, double angulo) {
+		this.distancia = distancia;
+		this.angulo = angulo;
+	}
+	
+	public double calcularDistancia() {
+		
+		double resultadoDistancia=0;
+		
+		if(this.validarAngulo()) {
+			
+			if(this.angulo >= anguloMinimo100 && this.angulo <= anguloMaximo100) {
+				resultadoDistancia = distancia;
+			}
+			else {
+				resultadoDistancia = distancia * porcentajeDistancia;
+			}
+		}
+			
+		return resultadoDistancia;	
+	}
+	
+	public boolean validarAngulo() {
+		
+		if(this.angulo>=anguloMinimoValido && this.angulo<=anguloMaximoValido)
+			return true;
+		return false;
+	}
 }
