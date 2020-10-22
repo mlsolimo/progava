36cd10708fa811d6943c00a7d59bfdc2325a1e9a
KevinSanchezU <70634113+KevinSanchezU@users.noreply.github.com>
2020-10-21 22:55:30 -0300

    Agregados métodos leerYProcesar y escribir
    utilice un constructor por defecto de Participantes y getters de Competencia

--- Caber/src/caber/Archivo.java
+++ Caber/src/caber/Archivo.java
@@ -1,5 +1,73 @@
 package caber;
 
-public class Archivo {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Locale;
+import java.util.Scanner;
 
+public class Archivo {
+	private String nombre;
+
+	public Archivo(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Competencia leerYProcesar() {
+		Scanner arch = null;
+		Competencia comp = new Competencia();
+		try {
+
+			arch = new Scanner(new File(this.nombre + ".in"));
+			arch.useLocale(Locale.ENGLISH);
+
+			int cantParticipantes = arch.nextInt();
+			double distancia, angulo;
+			for (int i = 0; i < cantParticipantes; i++) {
+				Participante p = new Participante();
+				for (int j = 0; j < 3; j++) {
+
+					distancia = arch.nextDouble();
+					angulo = arch.nextDouble();
+					Lanzamiento lanz = new Lanzamiento(distancia, angulo);
+					p.agregarLanzamiento(lanz);
+				}
+				comp.agregar(p);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			arch.close();
+		}
+		return comp;
+	}
+
+	public void escribir(Competencia comp) {
+		FileWriter arch = null;
+		PrintWriter pw = null;
+
+		comp.obtenerPodioConsistencia();
+		comp.obtenerPodioDistancia();
+
+		try {
+			arch = new FileWriter(this.nombre + ".out");
+			pw = new PrintWriter(arch);
+
+			pw.println(comp.getMayorConsistencia());
+			pw.println(comp.getPodioDistancia());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (arch != null) {
+				try {
+					arch.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
