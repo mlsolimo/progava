068e55ad952b8ef4a8c7372a90e64f0946db9430
KevinSanchezU <70634113+KevinSanchezU@users.noreply.github.com>
2020-11-02 14:33:03 -0300

    TestLanzamiento

--- Caber/src/caber/LanzamientoTest.java
+++ Caber/src/caber/LanzamientoTest.java
@@ -4,31 +4,50 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 @SuppressWarnings("deprecation")
 public class LanzamientoTest {
 
 	@Test
 	public void testLanzamientoPerfecto() {
-		Lanzamiento l1 = new Lanzamiento(1.0,5.0);
+		Lanzamiento l1 = new Lanzamiento(1.0, 5.0);
 		double distancia = l1.calcularDistancia();
-		
-		assertEquals(1.0, distancia);
+
+		Assert.assertEquals(1.0, distancia);
 	}
 
 	@Test
 	public void testLanzamientoAceptable() {
-		Lanzamiento l1 = new Lanzamiento(1.0,35);
+		Lanzamiento l1 = new Lanzamiento(1.0, 35);
 		double distancia = l1.calcularDistancia();
-		
-		assertEquals(0.8, distancia);
+
+		Assert.assertEquals(0.8, distancia);
 	}
-	
+
 	@Test
 	public void testLanzamientoDescalificado() {
-		Lanzamiento l1 = new Lanzamiento(1.0,100);
+		Lanzamiento l1 = new Lanzamiento(1.0, 100);
 		double distancia = l1.calcularDistancia();
-		
-		assertEquals(0.0, distancia);
+
+		System.out.println(distancia);
+		System.out.println(0.0);
+		Assert.assertEquals(0.0, distancia);
+	}
+
+	@Test
+	public void testAnguloNoValido() {
+		Lanzamiento l1 = new Lanzamiento(1.0, 100);
+		boolean angulo = l1.validarAngulo();
+
+		assertEquals(false, angulo);
+	}
+
+	@Test
+	public void testAnguloValido() {
+		Lanzamiento l1 = new Lanzamiento(1.0, 30);
+		boolean angulo = l1.validarAngulo();
+
+		assertEquals(true, angulo);
 	}
 }
-
