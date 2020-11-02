6b225bce63d49d4488647b1ec23490f753320e6d
KevinSanchezU <70634113+KevinSanchezU@users.noreply.github.com>
2020-11-02 14:25:23 -0300

    LanzamientoTest

--- Caber/src/caber/LanzamientoTest.java
+++ Caber/src/caber/LanzamientoTest.java
@@ -4,11 +4,31 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+@SuppressWarnings("deprecation")
 public class LanzamientoTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testLanzamientoPerfecto() {
+		Lanzamiento l1 = new Lanzamiento(1.0,5.0);
+		double distancia = l1.calcularDistancia();
+		
+		assertEquals(1.0, distancia);
 	}
 
+	@Test
+	public void testLanzamientoAceptable() {
+		Lanzamiento l1 = new Lanzamiento(1.0,35);
+		double distancia = l1.calcularDistancia();
+		
+		assertEquals(0.8, distancia);
+	}
+	
+	@Test
+	public void testLanzamientoDescalificado() {
+		Lanzamiento l1 = new Lanzamiento(1.0,100);
+		double distancia = l1.calcularDistancia();
+		
+		assertEquals(0.0, distancia);
+	}
 }
+
